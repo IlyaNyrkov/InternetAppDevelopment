5aa3d942a784fb633363bf5daa17ee2f846aa686
Ilya <ilya.nyrkov@gmail.com>
2021-09-04 21:40:34 +0300

    implemented field method

--- lab_python_fp/field.py
+++ lab_python_fp/field.py
@@ -5,8 +5,15 @@ def field(items, *args):
     result = []
     if len(args) == 1:
         for item in items:
-            result.append(item[args[0]])
+            value = item[args[0]]
+            if value is not None:
+                result.append(value)
     else:
         for item in items:
-            
-            result.append()
+            new_dict = dict()
+            for key_value in args:
+                value = item.get(key_value)
+                if value is not None:
+                    new_dict.update({key_value: value})
+            result.append(new_dict)
+    return result
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from lab_python_fp import field
+from lab_python_fp.field import field
 from lab_python_fp.gen_random import gen_random
 from lab_python_fp.sort import lambda_sort
 from lab_python_fp.sort import key_sort
@@ -7,10 +7,11 @@ from lab_python_fp.sort import key_sort
 def main():
     print("testing")
     goods = [{'title': 'Ковер', 'price': 2000, 'color': 'green'},
-             {'title': 'Диван для отдыха', 'price': 5000, 'color': 'black'}
+             {'title': 'Диван для отдыха', 'price': 5000, 'color': 'black'},
+             {'title': 'Железный трон', 'price': None, 'color': 'silver'}
              ]
-    #print(field(goods, 'title'))  # output: 'Ковер', 'Диван для отдыха'
-    #print(field(goods, 'title', 'price'))
+    print(field(goods, 'title'))  # output: 'Ковер', 'Диван для отдыха'
+    print(field(goods, 'title', 'price'))
     print(gen_random(5, 1, 3))
     test_array = [4, -30, 30, 100, -100, 123, 1, 0, -1, -4]
     print(key_sort(test_array))
