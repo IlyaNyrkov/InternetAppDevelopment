90657100505ef0285bf664ab3f238d5f855f60e3
Ilya <ilya.nyrkov@gmail.com>
2021-09-07 00:22:50 +0300

    added lab report, changed unique return type to list

--- lab_python_fp/unique.py
+++ lab_python_fp/unique.py
@@ -4,21 +4,13 @@ from collections import Counter
 
 class Unique(object):
     def __init__(self, items, **kwargs):
-        # Нужно реализовать конструктор
-        # В качестве ключевого аргумента, конструктор должен принимать bool-параметр ignore_case,
-        # в зависимости от значения которого будут считаться одинаковыми строки в разном регистре
-        # Например: ignore_case = True, Aбв и АБВ - разные строки
-        #           ignore_case = False, Aбв и АБВ - одинаковые строки, одна из которых удалится
-        # По-умолчанию ignore_case = False
         self.ignore_case = True
         if len(kwargs) != 0:
             self.ignore_case = kwargs['ignore_case']
-        # Can't use set here, because set sorts it's elements
-        # we want to save original order
         if not self.ignore_case:
-            self.elements = sorted(set([x.casefold() for x in items]))
+            self.elements = list(sorted(set([x.casefold() for x in items])))
         else:
-            self.elements = sorted(set(items))
+            self.elements = list(sorted(set(items)))
         self.elements_it = iter(self.elements)
 
     def __next__(self):
